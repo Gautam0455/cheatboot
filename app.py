ccc49d76136f0a04311f17f0d9b7518b90cdbf1e
Gautam0455 <boy749377@gmail.com>
2025-05-18 11:15:40 +0530

    first

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
+#demoedasghkdgasjk   sdknhns
 from keras.models import load_model
 model = load_model('model.h5')
 import json
